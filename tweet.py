766b59be082f79392413d1668def0fca10557daf
Rissa2019 <49465024+Rissa2019@users.noreply.github.com>
2019-08-16 09:38:33 +0800

    Update and rename twitterAPI.py to tweet.py

--- tweet.py
+++ tweet.py
@@ -1,7 +1,6 @@
-import tweepy
-import datetime
+import tweepy import datetime
 
-#authentication
+#authenticate
 consumer_key= 'QpofFWZncjIRUJUNSwNWcikHj'
 consumer_secret= '7ewZRAXZJeRfD49FglYzuirerka2RcMErFSWKMtkUmotz5ucc5'
 access_token='1161503197481054209-MfiuR0aEEdi47fsrKE9fF1x67YUx2x'
@@ -11,13 +10,12 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-#Set Parameters
 max_tweets = 1000000
 celebrities = ["@katyperry", "@justinbieber", "@BarackObama", "@rihanna", "@taylorswift13", "@ladygaga", "@TheEllenShow", "@cristiano", "@timberlake", "@ArianaGrande"]
-#set start date as current date 
-start_date = datetime.date(2019, 8, 14)
+# Since we're using the standard Twitter API, there is a rate limit of 180 requests per 15 mins and maximum 100 entries per request.
+# Therefore, to make this realistic, we are restricting the time frame to be 1 hour: 23:00 08/14/2019 - 00:00 08/15/2019
+start_time = datetime.datetime(2019, 8, 14, 23, 0)
 
-#query to get most tweeted celebrity and its most tweeted author
 def get_cele_tweeted_by_count(query):
     searched_tweets = []
     last_id = -1
@@ -26,11 +24,11 @@ def get_cele_tweeted_by_count(query):
     while len(searched_tweets) < max_tweets:
         count = max_tweets - len(searched_tweets)
         try:
-            new_tweets = api.search(q=query, count=count, max_id=str(last_id - 1))
+            new_tweets = api.search(q=query, until='2019-08-15', count=count, max_id=str(last_id - 1))
             if not new_tweets:
                 break
             for tweet in new_tweets:
-                if tweet.created_at.date() >= start_date:
+                if tweet.created_at >= start_time:
                     author = tweet.author.name
                     tweet_authors[author] = tweet_authors.get(author, 0) + 1
                     searched_tweets.append(tweet)
@@ -48,6 +46,7 @@ def get_cele_tweeted_by_count(query):
     if tweet_authors:
         import operator
         most_tweet_author = max(tweet_authors.iteritems(), key=operator.itemgetter(1))[0]
+        most_tweet_author = most_tweet_author.encode('utf8')
     else:
         most_tweet_author = None
     return len(searched_tweets), most_tweet_author
@@ -64,5 +63,3 @@ for c in celebrities:
     print "celebrity", c, "tweeted by", count, "times.", most_tweet_author, "tweeted the celetrity most."
 print "The celebrity who has been the most tweeted about in the specified time frame is", max_tweeted_c
 print most_tweeted_author, "tweeted the celebrity most."
-
-
